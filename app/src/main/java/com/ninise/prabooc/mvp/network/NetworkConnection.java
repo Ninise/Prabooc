967a3c889a044aded4b4b54119d565be068ed655
Ninise <nikitonze@hotmail.com>
2016-04-23 19:17:01 +0300

    RxNetwork added

---
+++ app/src/main/java/com/ninise/prabooc/mvp/network/NetworkConnection.java
@@ -0,0 +1,7 @@
+package com.ninise.prabooc.mvp.network;
+
+/**
+ * Created by ninise on 23.04.16.
+ */
+public class NetworkConnection {
+}
---
+++ app/src/main/java/com/ninise/prabooc/mvp/presenter/login/ILoginPresenter.java
@@ -0,0 +1,7 @@
+package com.ninise.prabooc.mvp.presenter.login;
+
+/**
+ * Created by ninise on 23.04.16.
+ */
+public interface ILoginPresenter {
+}
