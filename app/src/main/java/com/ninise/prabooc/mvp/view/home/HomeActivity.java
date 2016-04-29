4884302105d96831337f6ac99aee549df9e102ca
Ninise <nikitonze@hotmail.com>
2016-04-29 19:59:25 +0300

    Signout works when the user taps twice on back

--- app/src/main/java/com/ninise/prabooc/mvp/view/home/HomeActivity.java
+++ app/src/main/java/com/ninise/prabooc/mvp/view/home/HomeActivity.java
@@ -1,15 +1,18 @@
 package com.ninise.prabooc.mvp.view.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.jakewharton.rxbinding.support.design.widget.RxNavigationView;
 import com.jakewharton.rxbinding.support.v7.widget.RxToolbar;
 import com.ninise.prabooc.R;
+import com.ninise.prabooc.mvp.network.facebook.SessionState;
 import com.ninise.prabooc.mvp.presenter.home.HomePresenter;
 import com.ninise.prabooc.mvp.presenter.home.IHomeView;
 
@@ -23,6 +26,8 @@ public class HomeActivity extends AppCompatActivity implements IHomeView {
     @Bind(R.id.homeNavigationView) NavigationView mNavigationView;
 
     private HomePresenter mPresenter;
+
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +58,20 @@ public class HomeActivity extends AppCompatActivity implements IHomeView {
     protected void onDestroy() {
         ButterKnife.unbind(this);
         super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            SessionState.signOut();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, getString(R.string.back_pressed), Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
     }
 
     @Override
